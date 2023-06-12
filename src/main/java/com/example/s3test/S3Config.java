4d762ce59a4206e3c1906ffe77f6c1b6a9084186
sweun2 <sweun3@gmail.com>
2023-06-12 20:55:19 +0900

    feat: Change to aws academy -

--- src/main/java/com/example/s3test/S3Config.java
+++ src/main/java/com/example/s3test/S3Config.java
@@ -1,7 +1,9 @@
 package com.example.s3test;
 
+import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.transfer.TransferManager;
@@ -13,26 +15,15 @@ import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class S3Config {
-
-    @Value("${cloud.aws.credentials.access-key}")
-    public String accessKey;
-    @Value("${cloud.aws.credentials.secret-key}")
-    public String secretKey;
     @Value("${cloud.aws.region.static}")
     public String region;
 
-    @Bean
-    @Primary
-    public BasicAWSCredentials awsCredentialsProvider(){
-        BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(accessKey, secretKey);
-        return basicAWSCredentials;
-    }
 
     @Bean
     public AmazonS3 amazonS3() {
         AmazonS3 s3Builder = AmazonS3ClientBuilder.standard()
                 .withRegion(region)
-                .withCredentials(new AWSStaticCredentialsProvider(awsCredentialsProvider()))
+                .withCredentials(DefaultAWSCredentialsProviderChain.getInstance())
                 .build();
         return s3Builder;
     }
